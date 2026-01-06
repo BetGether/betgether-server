8a01cd8790c48b478a1bdbd34919805c260d700d
hyungyu-02 <yona20020209@gmail.com>
2026-01-06 15:53:28 +0900

    CHORE: Initial Spring Boot Setup

---
+++ src/main/java/com/betgether/betgether_server/BetgetherServerApplication.java
@@ -0,0 +1,13 @@
+package com.betgether.betgether_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetgetherServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetgetherServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betgether/betgether_server/BetgetherServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betgether.betgether_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetgetherServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
