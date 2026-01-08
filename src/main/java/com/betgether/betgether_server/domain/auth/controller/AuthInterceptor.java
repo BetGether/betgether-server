1c1db6f492f947167893b96487b86bcc788d5556
hyungyu-02 <yona20020209@gmail.com>
2026-01-09 02:35:20 +0900

    feat: set CORS Mapping

--- src/main/java/com/betgether/betgether_server/domain/auth/controller/AuthInterceptor.java
+++ src/main/java/com/betgether/betgether_server/domain/auth/controller/AuthInterceptor.java
@@ -4,9 +4,11 @@ import com.betgether.betgether_server.global.jwt.JwtProvider;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+@Component
 public class AuthInterceptor implements HandlerInterceptor {
     private final JwtProvider jwtProvider;
 
