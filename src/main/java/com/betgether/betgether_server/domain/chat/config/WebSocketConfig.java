4d517942cb9900a9b1f57f6d2e76d3725f5c30f6
YangJJune <58018839+YangJJune@users.noreply.github.com>
2026-01-09 16:55:07 +0900

    fix : 타입캐스트 에러 수정

--- src/main/java/com/betgether/betgether_server/domain/chat/config/WebSocketConfig.java
+++ src/main/java/com/betgether/betgether_server/domain/chat/config/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package com.betgether.betgether_server.domain.chat.config;
 
+import com.betgether.betgether_server.domain.chat.handler.HandshakeInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -8,7 +11,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final HandshakeInterceptor handshakeInterceptor;
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        // 인터셉터를 등록하여 메시지가 들어올 때 가로채도록 설정
+        registration.interceptors(handshakeInterceptor);
+    }
+
     // 엔드포인트 등록을 위한 설정
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
